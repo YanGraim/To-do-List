f34f14a64443475ae556ed645bdec94d4ba15400
YanGraim <yangraim@gmail.com>
2023-03-04 12:50:55 -0300

    Dark Mode, tela de login

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
         <h1>To do List</h1>
       </div>
       <v-spacer></v-spacer>
+      <v-btn icon @click="mudarTema">
+        <v-icon>mdi-theme-light-dark</v-icon>
+      </v-btn>
       <v-btn href="https://vuetifyjs.com/en/components/all/" target="_blank" text>
         <span class="mr-2">Vuetify</span>
         <v-icon>mdi-vuetify</v-icon>
@@ -18,6 +21,19 @@
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data: () => ({
+    darkMode: false,
+  }),
+  methods: {
+    mudarTema() {
+      if (this.darkMode) {
+        this.darkMode = false;
+      } else {
+        this.darkMode = true;
+      } 
+      this.$vuetify.theme.dark = this.darkMode;
+    }
+  }
 };
 </script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-container>
+        <div id="card">
+            <v-avatar color="primary">
+                <v-icon dark large>
+                    mdi-account
+                </v-icon>
+            </v-avatar>
+            <v-text-field name="name" label="login" id="id" style="width: 60%;"></v-text-field>
+            <v-text-field name="name" label="senha" id="id" style="width: 60%;" type="password"></v-text-field>
+            <v-btn>Entrar</v-btn>
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "LoginView",
+};
+</script>
+
+<style>
+#card {
+    background-color: #fffafa05;
+    border: 1px solid gray;
+    padding: 1rem;
+    max-width: 500px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+</style>
